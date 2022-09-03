292016aa0b2e0cc15c38e2fe2a978bd929822458
wassimmallehwm <wassimmalleh.wm@gmail.com>
2022-09-03 23:28:47 +0100

    choose role on login (for multi roles accounts)

--- client/src/components/modules/auth/Login.tsx
+++ client/src/components/modules/auth/Login.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useContext } from 'react'
 import { useNavigate } from 'react-router-dom';
-import { Button, Form } from 'semantic-ui-react';
 import { AuthContext } from '../../../contexts/auth/AuthContext';
 import { authenticate } from './auth.service';
 //import './login.css'
 import logo from '../../../assets/logo.png'
 import { SocketContext } from 'src/contexts/socket/SocketContext';
+import { Button } from '../../../shared/components';
 
 const Login = () => {
     const { login } = useContext(AuthContext)
@@ -16,6 +16,9 @@ const Login = () => {
         password: ''
     })
     const [loading, setLoading] = useState(false)
+    const [rolesModal, setRolesModal] = useState(false)
+    const [rolesList, setRolesList] = useState([])
+    const [loginResp, setLoginResp] = useState<any>()
 
     const { email, password } = loginInfo;
 
@@ -24,22 +27,61 @@ const Login = () => {
         setLoginInfo({ ...loginInfo, [name]: value })
     }
 
+    const onLogin = (data: any) => {
+        login(data)
+        const user_roles = data.roles.map((elem: any) => elem.label)
+        connect(data._id, user_roles)
+        navigate('/')
+    }
+
     const onSubmit = (e: any) => {
         e.preventDefault();
         authenticate(loginInfo).then(
             res => {
-                login(res.data)
-                const user_roles = res.data.roles.map((elem: any) => elem.label)
-                connect(res.data._id, user_roles)
-                navigate('/')
+                if (res.data.roles.length > 1) {
+                    setRolesModal(true)
+                    setRolesList(res.data.roles)
+                    setLoginResp(res.data)
+                } else {
+                    onLogin(res.data)
+                }
             },
             error => {
                 console.log("ERROR", error.response.data.msg);
             }
         )
     }
+
+    const continueAs = (id: string) => {
+        let loginData = loginResp
+        loginData.roles = [rolesList.find((elem: any) => elem._id == id)]
+        setRolesModal(false)
+        setRolesList([])
+        setLoginResp(null)
+        onLogin(loginData)
+    }
+
+    const _rolesModal = rolesModal && (
+        <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-80'>
+            <div className='bg-slate-100 p-16 rounded-sm'>
+            <div className='text-xl text-center text-gray-600 my-8 mx-auto'>
+                Continue as :
+            </div>
+            <div className='flex items-center gap-4'>
+                {
+                    rolesList.map((role: any) => (
+                        <Button color='primary' outline onClick={() => continueAs(role._id)}>
+                            {role.label}
+                        </Button>
+                    ))
+                }
+            </div>
+            </div>
+        </div>
+    )
     return (
         <div className="min-h-screen bg-slate-100 flex flex-col justify-center">
+            {_rolesModal}
             <div className="max-w-md w-full mx-auto flex flex-col items-center">
                 <img src={logo} className="w-32 text-center" />
                 <div className="text-3xl font-bold text-gray-800 text-center">
@@ -57,7 +99,7 @@ const Login = () => {
                     </div>
                     <div>
                         <label className="text-sm font-bold text-gray-600 block">
-                            Mot de passe
+                            Password
                         </label>
                         <input type="password" name="password" onChange={onChange} value={password}
                             className="w-full p-2 border border-gray-300 rounded mt-1" />
@@ -65,7 +107,7 @@ const Login = () => {
                     <div className="flex items-center justify-between">
                         <div>
                             <a href="#" className="font-medium text-sm text-primary-500">
-                                Mot de passe oublié ?
+                                Forgot password ?
                             </a>
                         </div>
                     </div>
