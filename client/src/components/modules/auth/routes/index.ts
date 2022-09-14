dee3717a656650a58377bbf54e238cc2cd6aead9
wassimmallehwm <wassimmalleh.wm@gmail.com>
2022-09-14 23:10:37 +0100

    signup page route

--- client/src/components/modules/auth/routes/index.ts
+++ client/src/components/modules/auth/routes/index.ts
@@ -1,12 +1,19 @@
 import { lazy } from 'react';
 const Login = lazy(() => import('../Login'));
+const Signup = lazy(() => import('../signup/Signup'));
 
 const authRoutes: any[] = [
+    {
+        path: "/signup",
+        component: Signup,
+        status: 'GUEST'
+    },
     {
         path: "/login",
         component: Login,
         status: 'GUEST'
-    },
+    }
+    
 ]
 
 export default authRoutes;
